7171f863bdabf207c13aa7c670e802f1325ba918
My PC <azamatotaboev7621@gmail.com>
2025-06-16 21:00:27 +0500

    project was created

---
+++ src/main/java/uz/dckroff/statisfy/StatisfyApplication.java
@@ -0,0 +1,13 @@
+package uz.dckroff.statisfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatisfyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatisfyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/dckroff/statisfy/StatisfyApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.dckroff.statisfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisfyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
